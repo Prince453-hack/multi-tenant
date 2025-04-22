9446ee99b3943ac1606f787aa9816b01e02a39be
Random <tabishnizami456@gmail.com>
2025-04-22 15:39:13 +0530

    Components Customizations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,29 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return (
-    <div className="">
-      <p className="text-rose-500">This is a test</p>
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
+        <div className="">
+          <Button variant="elevated">Button</Button>
+        </div>
+        <div className="">
+          <Input placeholder="Input" />
+        </div>
+        <div className="">
+          <Progress value={50} />
+        </div>
+        <div className="">
+          <Textarea placeholder="This is a textarea" />
+        </div>
+        <div className="">
+          <Checkbox />
+        </div>
+      </div>
     </div>
   );
 }
